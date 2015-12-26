ecb3a723ca02d6c0cfa0a327a05d618de2b75141
Omer Ben-Amram <omerbenamram@gmail.com>
2015-12-27 00:01:21 +0200

    oops

--- src/pyzap/categories.py
+++ src/pyzap/categories.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, unicode_literals, print_function, division
-# -*- coding: <utf-8> -*-
+# -*- coding: utf-8 -*-
+
 
 class ZapCategories:
     class Electronics:
--- src/pyzap/zap.py
+++ src/pyzap/zap.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, unicode_literals, print_function, division
-# -*- coding: <utf-8> -*-
+# -*- coding: utf-8 -*-
 import itertools
 import re
 import sys
