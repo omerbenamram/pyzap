926d6547b40d62386cd2d291f6171cd529cbfafc
Omer Ben-Amram <omerbenamram@gmail.com>
2015-12-30 22:28:03 +0200

    pies

--- src/pyzap/categories.py
+++ src/pyzap/categories.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, unicode_literals, print_function, divisi
 from collections import namedtuple
 from fuzzywuzzy import process
 
+from pies.overrides import *
+
 ZapCategory = namedtuple('ZapCategory', ['category', 'hebrew_description', 'english_description'])
 
 
