e6ddae7a65ffdd043cb52eb3247a20550b511010
Omer Ben-Amram <omerbenamram@gmail.com>
2016-03-16 00:28:05 +0200

    possible fix for py2.7 linux

--- src/pyzap/categories.py
+++ src/pyzap/categories.py
@@ -1,5 +1,5 @@
-from __future__ import absolute_import, unicode_literals, print_function, division
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals, print_function, division
 from collections import namedtuple
 from fuzzywuzzy import process
 
